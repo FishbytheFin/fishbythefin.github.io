020f6ff818dd845add0f18c0a7155900993538e4
FishbytheFin <62949492+FishbytheFin@users.noreply.github.com>
2024-01-28 20:54:28 -0800

    Add files via upload

--- diffyq/script.js
+++ diffyq/script.js
@@ -1,21 +1,33 @@
-const hints = ["fart", "poop", "pee", "peepee", "peepeepoopoo"];
+const hints = ["Hint #1: Teachers wouldn't like you interupting their class.", "Hint #2: You really need a hint for this? Too bad.", "Hint #3: You have the keys, you can find the car buy making it honk."];
+const videos = ["videos/54.mp4", "videos/16.mp4", "videos/38.mp4"];
+const videoTimes = [54, 16, 38];
+const hintDiv = document.getElementById("hint");
 
-function playAd1() {
+function playAd0() {
+  
+  const text1 = document.createElement('div');
+  const adText = document.createTextNode("A short ad will play before recieving the next hint:");
+  text1.appendChild(adText);
+  hintDiv.appendChild(text1);
+  
   const video = document.createElement('video');
-  video.src = 'videos/boots.mov';
+  let index = Math.floor(Math.random()*videos.length);
+  video.src = videos[index];
   video.controls = false;
-  document.body.appendChild(video);
+  hintDiv.appendChild(video);
   video.play();
+  
   setTimeout(() => {
     reveal(video, hints[0]);
-  }, 30000);
+  }, (1000*videoTimes[index]));
 }
 
 function reveal(video, hint) {
   video.pause();
   video.display = 'none';
+  video.remove();
   const text = document.createElement('div');
   const hintText = document.createTextNode(hint);
   text.appendChild(hintText);
-  document.body.appendChild(text);
+  hintDiv.appendChild(text);
 }
